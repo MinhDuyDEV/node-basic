a23f7663ede786a82875190d087cfefca82f281a
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2023-01-04 13:10:52 +0700

    connect db, ejs loop

--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
 import express from "express";
 import configViewEngine from "./configs/viewEngine";
+import initWebRoute from "./router/web";
+// import connection from "./configs/connectDB";
+require("dotenv").config();
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// setup view engine
 configViewEngine(app);
-app.get("/", (req, res) => {
-  res.render("index.ejs");
-});
-
-app.get("/about", (req, res) => {
-  res.send(`I'm Duy`);
-});
+// init web route
+initWebRoute(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
